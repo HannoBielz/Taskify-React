36304695a7a7661afeca8650d0b0b72e4fef0921
Hanno Bielz <bielzhanno@gmail.com>
2024-04-23 17:24:02 +0200

    added functionality to inputfield and add button

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TaskInput() {
+  const [taskInputValue, setTaskInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setTaskInputValue(event.target.value);
+  };
+
+  const addTask = () => {
+    if (taskInputValue.trim() !== "") {
+      console.log("Add Task");
+
+      // Hier wird der onTaskAdd-Prop erstellt und die neue Aufgabe übergeben
+      if (typeof onTaskAdd === "function") {
+        onTaskAdd(taskInputValue);
+      }
+
+      // Leeren Sie das Eingabefeld
+      setTaskInputValue("");
+    } else {
+      console.log("Ixnput is empty");
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  };
+
   return (
     <div className="input-container">
-      <input id="todo-input" type="text" placeholder="Enter a task ..." />
-      <button id="add-button">Add</button>
+      <input
+        id="todo-input"
+        type="text"
+        placeholder="Enter a task ..."
+        value={taskInputValue}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+      />
+      <button id="add-button" onClick={addTask}>
+        Add
+      </button>
     </div>
   );
 }
