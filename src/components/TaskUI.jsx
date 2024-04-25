0af1d4ca16b706c2e40f68f25c0f08b3cb725e42
Sebastian Weber <s-weber85@gmx.de>
2024-04-25 15:12:41 +0200

    task Ui update

--- src/components/TaskUI.jsx
+++ src/components/TaskUI.jsx
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+import TaskInput from "./TaskInput";
+import TaskCard from "./TaskCard";
+
 function TaskUI() {
 
+    
+const addTask = () => {
+    if (taskInputValue.trim() !== "") {
+      console.log("Add Task");
+      onTaskAdd(taskInputValue);
+
+      setTaskInputValue("");
+    } else {
+      console.log("Ixnput is empty");
+    }
+  };
 
   return (
 
@@ -12,11 +27,11 @@ function TaskUI() {
     <div id="container">
       <div className="input-container">
         <input id="todo-input" type="text" placeholder="Enter a task ..." />
-        <button id="add-button">Add</button>
+        <button id="add-button" onClick={addTask}>Add</button>
       </div>
       <div className="seperator" />
       <ul id="todo-list" />
-      <button id="delete-all-button">Delete all</button>
+      {/* <button id="delete-all-button" onClick={}>Delete all</button> */}
     </div>
     <button id="print-button">
       <img src="./images/print.jpeg" alt="Print" width="20px" />
